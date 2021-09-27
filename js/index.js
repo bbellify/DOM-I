36a8979317d6a00cf15c86d8f212ecf7d0500550
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-09-27 12:49:29 -0700

    main content populated

--- js/index.js
+++ js/index.js
@@ -64,3 +64,35 @@ ctaH1.textContent = siteContent.cta.h1;
 ctaButton.textContent = siteContent.cta.button;
 ctaImg.src = siteContent.cta["img-src"];
 
+
+// main content
+// main content top
+const featuresH4 = document.querySelector('.top-content div:nth-child(1) h4');
+const featuresText = document.querySelector('.top-content div:nth-child(1) p');
+featuresH4.textContent = siteContent["main-content"]["features-h4"];
+featuresText.textContent = siteContent["main-content"]["features-content"];
+
+const aboutH4 = document.querySelector('.top-content div:nth-child(2) h4');
+const aboutText = document.querySelector('.top-content div:nth-child(2) p');
+aboutH4.textContent = siteContent["main-content"]["about-h4"];
+aboutText.textContent = siteContent["main-content"]["about-content"];
+
+// main content image
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = siteContent["main-content"]['middle-img-src'];
+
+// main content bottom
+const servicesH4 = document.querySelector('.bottom-content div:nth-child(1) h4');
+const servicesText = document.querySelector('.bottom-content div:nth-child(1) p');
+const productH4 = document.querySelector('.bottom-content div:nth-child(2) h4');
+const productText = document.querySelector('.bottom-content div:nth-child(2) p');const visionH4 = document.querySelector('.bottom-content div:nth-child(3) h4');
+const visionText = document.querySelector('.bottom-content div:nth-child(3) p');
+servicesH4.textContent = siteContent['main-content']['services-h4'];
+servicesText.textContent = siteContent['main-content']['services-content'];
+productH4.textContent = siteContent['main-content']['product-h4'];
+productText.textContent = siteContent['main-content']['product-content'];
+visionH4.textContent = siteContent['main-content']['vision-h4'];
+visionText.textContent = siteContent['main-content']['vision-content'];
+
+
+// contact section
